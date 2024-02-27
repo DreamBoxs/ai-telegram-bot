ac8346c78a7614526d12e7c782c6e8304ad06edf
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-28 02:46:21 +0700

    remove parameter and query

--- main.py
+++ main.py
@@ -25,19 +25,7 @@ def google_ai(question):
     if not AI_GOOGLE_API:
         return "Silakan periksa AI_GOOGLE_API Anda di file env"
     genai.configure(api_key=AI_GOOGLE_API)
-    generation_config = {
-        "temperature": 1,
-        "top_p": 1,
-        "top_k": 1,
-        "max_output_tokens": 2048,
-    }
-    safety_settings = [
-        {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-        {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-        {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-        {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-    ]
-    model = genai.GenerativeModel(model_name="gemini-1.0-pro", generation_config=generation_config, safety_settings=safety_settings)
+    model = genai.GenerativeModel(model_name="gemini-1.0-pro")
     convo = model.start_chat(history=[])
     convo.send_message(question)
     return convo.last.text
